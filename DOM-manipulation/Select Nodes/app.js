678a3a455553f68e4a14a67fe00f06a468a80d68
NaqeebH <Naqeeb.1011@gmail.com>
2026-06-04 12:32:38 +0200

    feat(dom): add DOM node selection examples

---
+++ DOM-manipulation/Select Nodes/app.js
@@ -0,0 +1,47 @@
+// DOM Manipulation 
+
+
+
+
+// GetElementByID()
+
+const title = document.getElementById('main-heading');
+
+console.log(title);
+
+
+// // GetElementByClassName()
+
+const listItems =
+  document.getElementsByClassName('list-items')
+  
+  console.log(listItems);
+
+
+// GetElementByTagName()
+
+const tagNames = document.getElementsByTagName('li');
+
+console.log(tagNames);
+
+
+// querySelector()
+
+const container = document.querySelector('div');
+
+console.log(container);
+
+
+// querySelectorAll()
+
+const allDivs = document.querySelectorAll('div');
+
+console.log(allDivs);
+
+
+
+
+
+
+
+
