047b68143316b3369da4b07cd038aca3ae3ae2a0
NaqeebH <Naqeeb.1011@gmail.com>
2026-06-04 11:56:07 +0200

    feat(events): add reveal content toggle example

--- DOM-manipulation/Event Listeners/index.js
+++ DOM-manipulation/Event Listeners/index.js
@@ -23,3 +23,19 @@ function changeBgColor() {
 newBackgroundColor.addEventListener("mouseover", changeBgColor);
 
 
+// Reveal Event 
+
+const revealBtn = document.querySelector('.reveal-btn');
+
+const hiddenContent = document.querySelector('.hidden-content');
+
+function revealContent() {
+    if (hiddenContent.classList.contains('reveal-btn')
+    ) {
+        hiddenContent.classList.remove('reveal-btn')
+    } else {
+        hiddenContent.classList.add('reveal-btn')
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
