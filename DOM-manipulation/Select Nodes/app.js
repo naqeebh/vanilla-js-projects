05aff663fad9df6aa4ce3927fe33fb890e5008ad
NaqeebH <Naqeeb.1011@gmail.com>
2026-06-05 09:57:22 +0200

    feat(dom): create and modify list item element

--- DOM-manipulation/Select Nodes/app.js
+++ DOM-manipulation/Select Nodes/app.js
@@ -50,13 +50,42 @@
 
 // Styling Elements
 
-const listItems = document.querySelectorAll('.list-items');
+// const listItems = document.querySelectorAll('.list-items');
 
-for (i = 0; i < listItems.length; i++) {
-    listItems[i].style.fontSize = '5rem';
-}
+// for (i = 0; i < listItems.length; i++) {
+//     listItems[i].style.fontSize = '5rem';
+// }
 
-console.log(listItems);
+// console.log(listItems);
+
+
+// Creating Elements 
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+
+// Adding elements
+
+ul.append(li)
+
+// Modifying the text
+
+li.innerText = "X-Men";
+
+
+// Modifying attributes and classes 
+
+li.setAttribute('id', 'main-heading');
+// li.removeAttribute('id');
+
+const title = document.querySelector('#main-heading');
+
+
+console.log(title.getAttribute('id'));
+
+
+
 
 
 
